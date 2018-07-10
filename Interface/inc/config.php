f69474c6062a0d74dc9deff6a2b6e01f868ced54
adsonileon <ileonrd@gmail.com>
2018-07-10 11:30:45 -0300

    Interface atualizada

--- Interface/inc/config.php
+++ Interface/inc/config.php
@@ -1,5 +1,8 @@
 <?php
 
+if(session_status()!=PHP_SESSION_ACTIVE)
+	session_start();
+
 /** caminho absoluto para a pasta do sistema **/
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
@@ -10,5 +13,10 @@ if ( !defined('BASEURL') )
 
 define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
 define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
+
+function clear_messages(){
+	$_SESSION['message']="";
+    $_SESSION['type']="";
+}
 
 ?>
